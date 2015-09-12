22b3a4698be1530a7d77292784cb1cd7c0838c87
Katie Richter <kayjanric1229@gmail.com>
2015-09-12 12:53:17 -0500

    Fixed Syntax Errors

--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -4,17 +4,29 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
              [hiccup.page :as page]
             [hiccup.form :as form]
-            [ring.middleware.params :refer [wrap-params]]))
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.adapter.jetty :as jetty]
+            [ring.util.anti-forgery :as anti-forgery]
+            [environ.core :refer [env]]))
 
 (def chat-messages
   (atom '()))
+
+(defn init []
+  (println "chatter is starting"))
+
+(defn destroy []
+  (println "chatter is shutting down"))
 
 (defn generate-message-view
   "This generates the HTML for displaying messages"
   [messages]
   (page/html5
    [:head
-    [:title "chatter"]]
+    [:title "chatter"]
+    (page/include-css "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css")
+    (page/include-js  "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js")
+    (page/include-css "/chatter.css")]
    [:body
     [:h1 "Our Chat App"]
     [:p
@@ -24,7 +36,7 @@
       "Message: " (form/text-field "msg")
       (form/submit-button "Submit"))]
     [:p
-     [:table
+     [:table#messages.table.table-bordered.table-striped
       (map (fn [m] [:tr [:td (:name m)] [:td " - "] [:td (:message m)]]) messages)]]]))
 
 (defn update-messages!
@@ -40,6 +52,11 @@
               new-messages (update-messages! chat-messages name-param msg-param)]
         (generate-message-view new-messages)
           ))
+  (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app (wrap-params app-routes))
+
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (jetty/run-jetty #'app {:port port :join? false})))
